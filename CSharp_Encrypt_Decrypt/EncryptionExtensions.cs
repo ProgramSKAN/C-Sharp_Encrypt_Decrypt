0c033f093f6559ae5ecfe419755c79cb443456ce
SandeepKandagatla <kandagatlas@gmail.com>
2020-05-12 18:07:32 +0530

    AES Encrypt Decrypt

---
+++ CSharp_Encrypt_Decrypt/EncryptionExtensions.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CSharp_Encrypt_Decrypt
+{
+    public static class EncryptionExtensions
+    {
+        private const string EncryptionKey = "This is secret key";
+        public static string EncryptText(this string textToEncrypt)
+        {
+            //get bytes of the string
+            byte[] bytesToBeEncrypted=Encoding.UTF8.GetBytes(textToEncrypt);
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(EncryptionKey);
+
+            //Hash password with SHA256
+            passwordBytes = SHA256.Create().ComputeHash(passwordBytes);
+
+            byte[] bytesEncrypted = AESEncrypt(bytesToBeEncrypted, passwordBytes);
+
+            string result = Convert.ToBase64String(bytesEncrypted);
+
+            return result;
+        }
+        public static string DecryptText(this string encryptedString)
+        {
+            //get bytes of the string
+            byte[] bytesToBeDecrypted = Convert.FromBase64String(encryptedString);
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(EncryptionKey);
+
+            //Hash password with SHA256
+            passwordBytes = SHA256.Create().ComputeHash(passwordBytes);
+
+            byte[] bytesDecrypted = AESDecrypt(bytesToBeDecrypted, passwordBytes);
+
+            string result = Encoding.UTF8.GetString(bytesDecrypted);
+
+            return result;
+        }
+
+        private static byte[] AESEncrypt(byte[] bytesToBeEncrypted, byte[] passwordBytes)
+        {
+            byte[] encryptedBytes = null;
+
+            //saltbytes must be atleast 8 bytes
+            byte[] saltBytes = { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            using(MemoryStream ms=new MemoryStream())
+            {
+                using(Aes aes = Aes.Create())
+                {
+                    aes.KeySize = 256;
+                    aes.BlockSize = 128;
+
+                    var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000);
+                    aes.Key = key.GetBytes(aes.KeySize / 8);
+                    aes.IV = key.GetBytes(aes.BlockSize / 8);
+                    aes.Mode = CipherMode.CBC;
+
+                    using(var cs=new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytesToBeEncrypted, 0, bytesToBeEncrypted.Length);
+                        cs.Flush();
+                        cs.Dispose();
+                    }
+                    encryptedBytes = ms.ToArray();
+                }
+            }
+            return encryptedBytes;
+        }
+        private static byte[] AESDecrypt(byte[] bytesToBeDecrypted, byte[] passwordBytes)
+        {
+            byte[] decryptedBytes = null;
+
+            //saltbytes must be atleast 8 bytes
+            byte[] saltBytes = { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (Aes aes = Aes.Create())
+                {
+                    aes.KeySize = 256;
+                    aes.BlockSize = 128;
+
+                    var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000);
+                    aes.Key = key.GetBytes(aes.KeySize / 8);
+                    aes.IV = key.GetBytes(aes.BlockSize / 8);
+                    aes.Mode = CipherMode.CBC;
+
+                    using (var cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytesToBeDecrypted, 0, bytesToBeDecrypted.Length);
+                        cs.Flush();
+                        cs.Dispose();
+                    }
+                    decryptedBytes = ms.ToArray();
+                }
+            }
+            return decryptedBytes;
+        }
+    }
+}
---
+++ CSharp_Encrypt_Decrypt/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace CSharp_Encrypt_Decrypt
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("AES Encyption Decryption Wizard");
+            Console.WriteLine("================================");
+            Console.WriteLine("Choose one of the following options");
+            Console.WriteLine("1.Encrypt Text");
+            Console.WriteLine("2.Decrypt Text");
+
+            string optionSelected = Console.ReadLine();
+
+            switch (Convert.ToInt32(optionSelected))
+            {
+                case 1:
+                    EncryptText();
+                    break;
+
+                case 2:
+                    DecryptText();
+                    break;
+            }
+            Console.ReadKey();
+        }
+        private static void EncryptText()
+        {
+            Console.Clear();
+            Console.Write("Enter Plain Text to Encrypt:");
+            string plainText = Console.ReadLine();
+            Console.WriteLine("Encrypted Text: "+ plainText.EncryptText());
+        }
+        private static void DecryptText()
+        {
+            Console.Clear();
+            Console.Write("Enter Plain Text to Decrypt:");
+            string plainText = Console.ReadLine();
+            try
+            {
+                Console.WriteLine("Decrypted Text: "+plainText.DecryptText());
+            }
+            catch
+            {
+                Console.WriteLine("Invalid Encrypted String");
+            }
+        }
+    }
+}
